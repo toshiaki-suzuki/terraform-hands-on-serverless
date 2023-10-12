d61a4278b9880e430768b1d53d73101206b2aa82
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-10-13 08:43:56 +0900

    iamロールを作成

--- envs/main.tf
+++ envs/main.tf
@@ -2,3 +2,9 @@ module "dynamodb" {
   source = "../modules/dynamodb"
   prefix = "sample1"
 }
+
+module "iam" {
+  source = "../modules/iam"
+  prefix = "sample1"
+  employee_list_table-arn = module.dynamodb.employee_list_table.arn
+}
---
+++ modules/iam/iam.tf
@@ -0,0 +1,55 @@
+variable "prefix" {
+  type = string
+}
+
+variable "employee_list_table-arn" {
+  type = string
+}
+
+# IAMロールを作成
+resource "aws_iam_role" "tr_lambda_role" {
+  name = "${var.prefix}_tr_lambda_role"
+  assume_role_policy = jsonencode({ # AssumeRoleポリシーをJSON形式で記述
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+# IAMロールにポリシーをアタッチ
+resource "aws_iam_role_policy_attachment" "tr_lambda_role_policy_attach" {
+  role       = aws_iam_role.tr_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# DynamoDBテーブルへのアクセス権限を付与するインラインポリシーを作成
+resource "aws_iam_role_policy" "tr_lambda_role_policy_policy" {
+  name = "${var.prefix}_tr_lambda_policy"
+  role = aws_iam_role.tr_lambda_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem"
+        ]
+        Resource = [
+          var.employee_list_table-arn
+        ]
+      }
+    ]
+  })
+}
+
+output "tr_lambda_role-arn" {
+  value = aws_iam_role.tr_lambda_role.arn
+}
