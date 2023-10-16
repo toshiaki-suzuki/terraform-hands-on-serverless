aab729d6e6987c24fa975fff0bd0c4b3ee491dde
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-10-16 16:38:58 +0900

    環境依存の変数を切り出し

--- envs/main.tf
+++ envs/main.tf
@@ -1,17 +1,17 @@
 module "dynamodb" {
   source = "../modules/dynamodb"
-  prefix = "sample1"
+  prefix = var.prefix
 }
 
 module "iam" {
   source = "../modules/iam"
-  prefix = "sample1"
+  prefix = var.prefix
   employee_list_table-arn = module.dynamodb.employee_list_table.arn
 }
 
 module "lambda" {
   source = "../modules/lambda"
-  prefix = "sample1"
+  prefix = var.prefix
   employee_list_table-name = module.dynamodb.employee_list_table.name
   tr_lambda_role-arn = module.iam.tr_lambda_role-arn
   tr_api-execution-arn = module.api_gateway.tr_api-execution-arn
@@ -19,6 +19,6 @@ module "lambda" {
 
 module "api_gateway" {
   source = "../modules/api-gateway"
-  prefix = "sample1"
+  prefix = var.prefix
   tr_lambda-invoke-arn = module.lambda.tr_lambda-invoke-arn
 }
